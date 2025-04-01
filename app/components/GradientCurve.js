bfcd7c8582f51f00a16548c1a4c7296c79622cc7
kritigaba710 <kritigaba31@gmail.com>
2025-04-01 09:03:19 -0700

    full

---
+++ app/components/GradientCurve.js
@@ -0,0 +1,10 @@
+// components/GradientLine.js
+import React from 'react';
+
+const GradientLine = () => {
+  return (
+    <div className="w-full h-2 bg-gradient-to-r from-pink-500 to-yellow-500 rounded-t-xl shadow-md mt-8"></div>
+  );
+};
+
+export default GradientLine;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import Head from "next/head";
+import GradientLine from "./components/GradientCurve";
 import { useRef, useState } from "react";
 
 export default function Home() {
@@ -12,9 +13,7 @@ export default function Home() {
   };
 
   return (
-    <div className="relative h-full w-full bg-slate-950 overflow-x-hidden">
-      <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-      <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+    <div className="relative h-full w-full overflow-hidden bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
 
       <Head>
         <title>Kriti - Portfolio</title>
@@ -45,100 +44,112 @@ export default function Home() {
         </header>
 
         <section id="about" className="py-20 pt-30 max-w-4xl mx-auto">
-  <h2 className="text-4xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">About Me</h2>
-  <div className="flex flex-col md:flex-row justify-center items-center mt-8">
-    {/* Image */}
-    <div className="rounded-full mx-6 my-4 w-60 h-60 overflow-hidden border-2 border-gray-300 mb-8 md:mb-0">
-      <Image src="./me.jpg" alt="image" className="w-full h-full object-cover" />
+          <h2 className="text-4xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">About Me</h2>
+          <div className="flex flex-col md:flex-row justify-center items-center mt-8">
+            {/* Image */}
+            <div className="rounded-full mx-6 my-4 w-60 h-60 overflow-hidden border-2 border-gray-300 mb-8 md:mb-0">
+              <Image src="/me.jpg" alt="image" width={100} height={100} className="w-full h-full object-cover" />
+            </div>
+            {/* Text */}
+            <div className="mt-4 flex flex-col w-2/3 text-center md:text-left">
+              <span className="text-3xl font-bold mx-2 text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">Hello, I&apos;m</span>
+              <span className="text-3xl mx-2">Kriti</span>
+              <p className="text-gray-500 text-sm mx-3">
+                I am a MERN stack Developer with a strong passion for building dynamic, scalable, and high-performance web applications. With expertise in MongoDB, Express.js, React, and Node.js, I specialize in developing responsive front-end interfaces and robust back-end systems. My front-end skills allow me to craft seamless and interactive user experiences using React.js, ensuring intuitive and engaging applications. On the backend, I leverage Node.js and Express.js to manage authentication and optimize server-side performance. From designing intuitive UIs using Spline, Figma to handling server-side logic and database operations, I excel in the full-stack development lifecycle, delivering modern web solutions that meet both technical and business needs.
+                I am currently pursuing a Bachelor's of Computers & Applications at Maharishi Markandeshwar University (2023 - 2026) with a CGPA of 8.0/10. My studies focus on advanced computer science concepts, including web development, algorithms, and data structures. I completed high school at Govt. model sanskriti senior seconday School (2017 - 2019) with a focus on mathematics and information technology, securing 87%.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="p-8">
+      <GradientLine />
     </div>
-    {/* Text */}
-    <div className="mt-4 flex flex-col w-2/3 text-center md:text-left">
-      <span className="text-3xl font-bold mx-2 text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">Hello, I&apos;m</span>
-      <span className="text-3xl mx-2">Kriti</span>
-      <p className="text-gray-500 text-sm mx-3">
-        I am a MERN stack Developer with a strong passion for building dynamic, scalable, and high-performance web applications. With expertise in MongoDB, Express.js, React, and Node.js, I specialize in developing responsive front-end interfaces and robust back-end systems. My front-end skills allow me to craft seamless and interactive user experiences using React.js, ensuring intuitive and engaging applications. On the backend, I leverage Node.js and Express.js to manage authentication and optimize server-side performance. From designing intuitive UIs using Spline, Figma to handling server-side logic and database operations, I excel in the full-stack development lifecycle, delivering modern web solutions that meet both technical and business needs.
-      </p>
+
+
+
+    <section id="skills" className="py-20 max-w-4xl mx-auto">
+  <h2 className="text-4xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">Skills</h2>
+  <div className="flex flex-col md:flex-row gap-6 items-center">
+    <div className="flex justify-center w-full md:w-1/2">
+      <Image src="/skill.svg" alt="Skill Image" width={200} height={200} />
+    </div>
+    <div className="flex flex-col w-full md:w-1/2">
+      {/* Frontend Technologies */}
+      <button
+        onClick={() => handleButtonClick(1)}
+        className="cursor-pointer flex hover:text-purple-800 items-center text-gray-300 mb-4"
+      >
+        Frontend Technologies
+        <Image width={32} height={32} src="/down.svg" alt="Down Arrow" className="invert-0 ml-2" />
+      </button>
+      <div ref={divRef} style={{ display: visibleDiv === 1 ? 'block' : 'none' }}>
+        <ul className="flex flex-wrap gap-6">
+          <li className=""><Image src="/html.svg" alt="" width={32} height={32} />HTML5</li>
+          <li><Image src="/css.avif" alt="" width={32} height={32} />CSS3</li>
+          <li className="mt-3"><Image src="/js.svg" alt="" width={32} height={32} />JavaScript</li>
+          <li><Image src="/tailwind.svg" alt="" width={32} height={32} />TailwindCSS</li>
+        </ul>
+      </div>
+      <hr />
+      
+      {/* Web Frameworks */}
+      <button
+        onClick={() => handleButtonClick(2)}
+        className="cursor-pointer hover:text-purple-800 flex items-center text-gray-300 mb-4"
+      >
+        Web Frameworks
+        <Image width={32} height={32} src="/down.svg" alt="Down Arrow" className="invert-0 ml-2" />
+      </button>
+      <div ref={divRef} style={{ display: visibleDiv === 2 ? 'block' : 'none' }}>
+        <ul className="flex flex-wrap gap-6">
+          <li><Image src="/express.svg" alt="" width={32} height={32} />Express</li>
+          <li><Image src="./react.svg" alt="" width={32} height={32} />React</li>
+          <li><Image src="/js.svg" alt="" width={32} height={32} />NodeJS</li>
+          <li><Image src="/next.svg" alt="" width={32} height={32} />NextJS</li>
+          <li><Image src="/vite.svg" alt="" width={32} height={32} />Vite</li>
+        </ul>
+      </div>
+      <hr />
+      
+      {/* Languages */}
+      <button
+        onClick={() => handleButtonClick(3)}
+        className="cursor-pointer hover:text-purple-800 flex items-center text-gray-300 mb-4"
+      >
+        Languages
+        <Image width={32} height={32} src="/down.svg" alt="Down Arrow" className="invert-0 ml-2" />
+      </button>
+      <div ref={divRef} style={{ display: visibleDiv === 3 ? 'block' : 'none' }}>
+        <ul className="flex flex-wrap gap-6">
+          <li><Image src="/js.svg" alt="" width={32} height={32} />JavaScript</li>
+          <li><Image src="/python.svg" alt="" width={32} height={32} />Python</li>
+          <li><Image src="/c++.svg" alt="" width={32} height={32} />C++</li>
+        </ul>
+      </div>
+      <hr />
+      
+      {/* Tools */}
+      <button
+        onClick={() => handleButtonClick(4)}
+        className="cursor-pointer hover:text-purple-800 flex items-center text-gray-300 mb-4"
+      >
+        Tools
+        <Image width={32} height={32} src="/down.svg" alt="Down Arrow" className="invert-0 ml-2" />
+      </button>
+      <div ref={divRef} style={{ display: visibleDiv === 4 ? 'block' : 'none' }}>
+        <ul className="flex flex-wrap gap-6">
+          <li><Image src="/vs.svg" alt="" width={32} height={32} />VS Code</li>
+          <li><Image src="/bash.svg" alt="" width={32} height={32} />BASH</li>
+          <li><Image src="/spline.jpg" alt="" width={32} height={32} />Spline</li>
+          <li><Image src="/figma.svg" alt="" width={32} height={32} />Figma</li>
+          <li><Image src="/npm.svg" alt="" width={32} height={32} />NPM</li>
+        </ul>
+      </div>
     </div>
   </div>
 </section>
 
-
-        {/* Education Section */}
-        <section id="education" className="py-20 max-w-4xl mx-auto">
-          <h2 className="text-4xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">Education</h2>
-          <div className="mt-6">
-            <div className="bg-gray-800 p-6 rounded-lg shadow-lg mb-6">
-              <h3 className="text-2xl">Bachelor&apos;s of Computers & Applications</h3>
-              <p className="mt-2">Maharishi Markandeshwar (Deemed to be University)  (2023 - 2026)</p>
-              <p className="text-gray-500">CGPA: 8.0/10</p>
-              <p className="mt-2 text-gray-400">Studying advanced concepts in computer science, focusing on web development, algorithms, and data structures.</p>
-            </div>
-            <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl">High School</h3>
-              <p className="mt-2">ABC High School (2017 - 2019)</p>
-              <p className="text-gray-500">Marks: 87%</p>
-              <p className="mt-2 text-gray-400">Completed high school with a focus on mathematics,and information technology.</p>
-            </div>
-          </div>
-        </section>
-
-        <section id="skills" className="py-20 max-w-4xl mx-auto ">
-          <h2 className="text-4xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">Skills</h2>
-          <div className="flex gap-36 items-center">
-            <div><Image src="./skill.svg" alt="" width={200} height={200} /></div>
-            <div className="flex flex-col">
-              <button onClick={() => handleButtonClick(1)} className="cursor-pointer flex hover:text-purple-800 items-center text-gray-300">Frontend Technologies <Image width={32} height={32} src="./down.svg" alt="Image" className="invert-0" /></button>
-              <div ref={divRef}
-                style={{ display: visibleDiv === 1 ? "block" : "none" }}>
-                <ul className="flex gap-6 items-center">
-                  <li className=""><Image src="./html.svg" alt="" width={32} height={32} />HTML5</li>
-                  <li><Image src="./css.avif" alt="" width={32} height={32} />CSS3</li>
-                  <li className="mt-3"><Image src="./js.svg" alt="" width={32} height={32} />Javascript</li>
-                  <li><Image src="./tailwind.svg" alt="" />TailwindCSS</li>
-                </ul>
-              </div>
-              <hr />
-
-
-              <button onClick={() => handleButtonClick(2)} className="cursor-pointer hover:text-purple-800  flex items-center  text-gray-300">Web Frameworks <Image width={32} height={32} src="./down.svg" alt="Image" className="invert-0" /></button>
-              <div ref={divRef}
-                style={{ display: visibleDiv === 2 ? "block" : "none" }}>
-                <ul className="flex gap-6 items-center">
-                  <li className=" "><Image src="./express.svg" alt="" width={32} height={32} />Express</li>
-                  <li><Image src="./react.svg" alt="" width={32} height={32} />React</li>
-                  <li className="mt-3"><Image src="./js.svg" alt="" width={32} height={32} />NodeJS</li>
-                  <li><Image src="./next.svg" alt="" />NextJS</li>
-                  <li><Image src="./vite.svg" alt="" />Vite</li>
-                </ul>
-              </div>
-              <hr />
-
-              <button onClick={() => handleButtonClick(3)} className="cursor-pointer hover:text-purple-800  flex items-center text-gray-300">Languages<Image width={32} height={32} src="./down.svg" alt="Image" className="invert-0" /></button>
-              <div ref={divRef}
-                style={{ display: visibleDiv === 3 ? "block" : "none" }}>
-                <ul className="flex gap-6 items-center">
-                  <li className=""><Image src="./js.svg" alt="" width={32} height={32} />Javascipt</li>
-                  <li><Image src="./python.svg" alt="" width={32} height={32} />Python</li>
-                  <li className="mt-3"><Image src="./c++.svg" alt="" width={32} height={32} />C++</li>
-                </ul>
-              </div>
-              <hr />
-
-
-              <button onClick={() => handleButtonClick(4)} className="cursor-pointer hover:text-purple-800  flex items-center text-gray-300">Tools <Image width={32} height={32} src="./down.svg" alt="Image" className="invert-0" /></button>
-              <div ref={divRef}
-                style={{ display: visibleDiv === 4 ? "block" : "none" }}>
-                <ul className="flex gap-6 items-center">
-                  <li className=""><Image src="./vs.svg" alt="" width={32} height={32} />VS Code</li>
-                  <li><Image src="./bash.svg" alt="" width={32} height={32} />BASH</li>
-                  <li className="mt-3"><Image src="./spline.jpg" alt="" width={32} height={32} />Spline</li>
-                  <li><Image src="./figma.svg" alt="" />Figma</li>
-                  <li><Image src="./npm.svg" alt="" />NPM</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </section>
 
         <section id="projects" className="py-20 max-w-4xl mx-auto">
           <h2 className="text-3xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600">Projects</h2>
